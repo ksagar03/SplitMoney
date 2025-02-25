c73deb81b0712d9c72031c78ef1a2f5b1842fa0f
ksagar03 <sagar22krishna03@gmail.com>
2025-02-25 22:53:43 +0530

    Updates: Created Composable component for adding multiple text field

--- app/src/main/java/com/example/splitmoney/signuporlogin/Authentication.kt
+++ app/src/main/java/com/example/splitmoney/signuporlogin/Authentication.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,15 +19,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.splitmoney.R
 import com.example.splitmoney.signuporlogin.components.TextComponent
+import com.example.splitmoney.signuporlogin.components.UserPasswordTextFiled
+import com.example.splitmoney.signuporlogin.components.UserTextFields
 
 @Composable
 fun SignUpScreen() {
-    Surface( color = colorResource(id = R.color.Dark_Theme),
+    Surface(
+        color = colorResource(id = R.color.Dark_Theme),
         modifier = Modifier
             .fillMaxSize()
 
     ) {
-        Column(modifier = Modifier.fillMaxSize().padding(28.dp)) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(28.dp)) {
             TextComponent(
                 value = stringResource(id = R.string.SignupHeading),
                 fontSize = 24.sp,
@@ -36,6 +42,22 @@ fun SignUpScreen() {
                 value = stringResource(id = R.string.SignupHeading2),
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(100.dp))
+            UserTextFields(
+                labelText = stringResource(id = R.string.userName),
+                iconData = painterResource(id = R.drawable.account_icon),
+                iconDescription = "userNameIcon"
+            )
+            UserTextFields(
+                labelText = stringResource(id = R.string.userEmail),
+                iconData = painterResource(id = R.drawable.outline_email_icon),
+                iconDescription = "userEmailIcon"
+            )
+            UserPasswordTextFiled(
+                labelText = stringResource(id = R.string.userPassword),
+                iconData = painterResource(id = R.drawable.baseline_lock_icon),
+                iconDescription = "userNameIcon"
             )
         }
 
--- app/src/main/java/com/example/splitmoney/signuporlogin/components/AppComponents.kt
+++ app/src/main/java/com/example/splitmoney/signuporlogin/components/AppComponents.kt
@@ -4,13 +4,32 @@ package com.example.splitmoney.signuporlogin.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -34,4 +53,83 @@ fun TextComponent(value: String, fontSize: TextUnit, fontWeight: FontWeight) {
         textAlign = TextAlign.Center
 
     )
+}
+
+@Composable
+fun UserTextFields(labelText: String, iconData: Painter, iconDescription: String) {
+    var textValue by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp)),
+        label = { Text(text = labelText) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = colorResource(id = R.color.Dark_Theme_Secondary),
+            focusedLabelColor = colorResource(id = R.color.teal_700),
+            cursorColor = colorResource(id = R.color.Dark_Theme_Text),
+            focusedContainerColor = colorResource(id = R.color.Dark_Theme_Primary),
+        ),
+        keyboardOptions = KeyboardOptions.Default,
+        value = textValue,
+        onValueChange = {
+            textValue = it
+        },
+        leadingIcon = {
+            Icon(painter = iconData, contentDescription = iconDescription)
+        }
+
+    )
+
+}
+
+@Composable
+fun UserPasswordTextFiled(labelText: String, iconData: Painter, iconDescription: String) {
+    var passwordValue by remember { mutableStateOf("") }
+
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp)),
+        label = { Text(text = labelText) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = colorResource(id = R.color.Dark_Theme_Secondary),
+            focusedLabelColor = colorResource(id = R.color.teal_700),
+            cursorColor = colorResource(id = R.color.Dark_Theme_Text),
+            focusedContainerColor = colorResource(id = R.color.Dark_Theme_Primary),
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+
+        value = passwordValue,
+        onValueChange = {
+            passwordValue = it
+        },
+        leadingIcon = {
+            Icon(painter = iconData, contentDescription = iconDescription)
+        },
+        trailingIcon = {
+            val iconImage = if (passwordVisible) {
+                ImageVector.vectorResource(id = R.drawable.visibility_icon)
+            } else {
+                ImageVector.vectorResource(id = R.drawable.visibility_off_icon)
+            }
+            val description = if (passwordVisible) {
+                "hide password"
+            } else {
+                "show password"
+            }
+
+
+            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                Icon(imageVector = iconImage, contentDescription = description)
+            }
+
+
+        },
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation()
+
+    )
+
 }
